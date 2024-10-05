bf13e66aa67cf1179e63afb750e915c30a25bcdb
flaviebou <55443901+flaviebou@users.noreply.github.com>
2024-10-05 17:06:25 +0100

    Update moveZero.js

--- moveZero.js
+++ moveZero.js
@@ -1,9 +1,5 @@
 function moveZeros(arr) {
-var result = arr;
-var resultzero=[];
-    while (result.indexOf(0)>-1){
-        result.splice(result.indexOf(0),1);
-        resultzero.push(0);
-    }
-    return result.concat(resultzero)
+  var result = arr.filter(function (x){return x!==0});
+  var zero = arr.filter(function (x){return x===0}); 
+  return result.concat(zero)
 }
